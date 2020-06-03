7427d5d1fbf68374abdbd2d83c35ef91eb8cc447
Chris Attaway <chris.d.attaway@gmail.com>
2020-06-02 20:32:41 -0500

    Updated using statements to match some namespace changes

--- HttpClientService.Test/HttpResponseDeserializationExtensionsShould.cs
+++ HttpClientService.Test/HttpResponseDeserializationExtensionsShould.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HttpClientService.Test.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text.Json;
@@ -109,22 +110,6 @@ namespace HttpClientService.Test
             catch (Exception exception)
             {
                 Assert.IsType<EmptyContentException>(exception);
-            }
-        }
-
-        private class TestClass : IComparable
-        {
-            public string A { get; set; }
-            public int B { get; set; }
-
-            public int CompareTo(object obj)
-            {
-                if (obj is TestClass other)
-                {
-                    return (this.A == other.A && this.B == other.B) ? 0 : this.B > other.B ? 1 : -1;
-                }
-
-                return -1;
             }
         }
     }
